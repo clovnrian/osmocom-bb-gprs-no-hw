bfcc9cc829c8fca0b2a3f0e3542efdd2fb7a889d
clovnrian <clovnrian@gmail.com>
2015-05-23 13:41:30 +0200

    fix segsegv error while sending RACH

--- src/host/bts_proxy/src/proxy/L1ToBts.c
+++ src/host/bts_proxy/src/proxy/L1ToBts.c
@@ -10,9 +10,8 @@
 #include <osmocom/core/gsmtap_util.h>
 #include <link_info.h>
 
-#define BTS_DST_IP '127.0.0.1'
-
-const int BTS_DST_PORT = 12345;
+const char *BTS_DST_IP = "127.0.0.1";
+const int BTS_DST_PORT = 18789;
 
 /** write message to mobile socket **/
 void write_to_bts(int socket_bts, struct msgb *msg){
@@ -22,10 +21,10 @@ void write_to_bts(int socket_bts, struct msgb *msg){
 	memset((char *) &bts, 0, sizeof(bts));
 	bts.sin_family = AF_INET;
 	bts.sin_port = htons(BTS_DST_PORT);
-        if (inet_aton(BTS_DST_IP, &bts.sin_addr)==0) {
-          fprintf(stderr, "inet_aton() failed\n");
-          exit(1);
-        }
+    if (inet_aton(BTS_DST_IP, &bts.sin_addr)==0) {
+    	fprintf(stderr, "inet_aton() failed\n");
+        exit(1);
+    }
 
 	/** send data to mobile **/
 	if (sendto(socket_bts, msg->data, msg->len, 0, &bts, sizeof(bts))==-1)
@@ -39,13 +38,7 @@ void write_to_bts(int socket_bts, struct msgb *msg){
 void l1_to_bts_rach_req(int socket_bts, struct l1ctl_info_dl *ul){
 	struct l1ctl_rach_req *rach_req = (struct l1ctl_rach_req *) ul->payload;
 	struct msgb *msg;
-	uint8_t data[2];
-	uint16_t info[1];
-
-	data[0] = 0;
-	data[1] = rach_req->ra;
-
-	info[0] = ((uint16_t)(data[0])) | ((uint16_t)(data[1])<<8);
+	uint8_t data[] = {0x80};
 
 	msg = gsmtap_makemsg(
 		  globalArfcn,
@@ -55,8 +48,8 @@ void l1_to_bts_rach_req(int socket_bts, struct l1ctl_info_dl *ul){
 		  0,
 		  0,
 		  0,
-		  info,
-		  sizeof(uint16_t)
+		  data,
+		  sizeof(data)
 		);
 
 	write_to_bts(socket_bts, msg);
